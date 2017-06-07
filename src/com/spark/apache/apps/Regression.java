1f194d4d34282ef904d65906f65b90da8d583b49
waldstein <wangbaohua2009@163.com>
2017-06-07 17:07:42 +0800

    Add Spark Support

---
+++ src/com/spark/apache/apps/Regression.java
@@ -0,0 +1,76 @@
+package com.spark.apache.apps;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import scala.Tuple2;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.Function;
+import org.apache.spark.api.java.function.Function2;
+import org.apache.spark.api.java.function.PairFunction;
+import org.apache.spark.mllib.regression.LabeledPoint;
+import org.apache.spark.mllib.tree.DecisionTree;
+import org.apache.spark.mllib.tree.model.DecisionTreeModel;
+import org.apache.spark.mllib.util.MLUtils;
+
+/**
+ * Created by baohuaw on 2017/6/7.
+ */
+public class Regression {
+    public static void main(String[] args) {
+        SparkConf sparkConf = new SparkConf().setAppName("JavaDecisionTreeRegressionExample");
+        JavaSparkContext jsc = new JavaSparkContext(sparkConf);
+
+// Load and parse the data file.
+        String datapath = "data/mllib/sample_libsvm_data.txt";
+        JavaRDD<LabeledPoint> data = MLUtils.loadLibSVMFile(jsc.sc(), datapath).toJavaRDD();
+// Split the data into training and test sets (30% held out for testing)
+        JavaRDD<LabeledPoint>[] splits = data.randomSplit(new double[]{0.7, 0.3});
+        JavaRDD<LabeledPoint> trainingData = splits[0];
+        JavaRDD<LabeledPoint> testData = splits[1];
+
+// Set parameters.
+// Empty categoricalFeaturesInfo indicates all features are continuous.
+        Map<Integer, Integer> categoricalFeaturesInfo = new HashMap<>();
+        String impurity = "variance";
+        Integer maxDepth = 5;
+        Integer maxBins = 32;
+
+// Train a DecisionTree model.
+        final DecisionTreeModel model = DecisionTree.trainRegressor(trainingData,
+                categoricalFeaturesInfo, impurity, maxDepth, maxBins);
+
+// Evaluate model on test instances and compute test error
+        JavaPairRDD<Double, Double> predictionAndLabel =
+                testData.mapToPair(new PairFunction<LabeledPoint, Double, Double>() {
+                    @Override
+                    public Tuple2<Double, Double> call(LabeledPoint p) {
+                        return new Tuple2<>(model.predict(p.features()), p.label());
+                    }
+                });
+        Double testMSE =
+                predictionAndLabel.map(new Function<Tuple2<Double, Double>, Double>() {
+                    @Override
+                    public Double call(Tuple2<Double, Double> pl) {
+                        Double diff = pl._1() - pl._2();
+                        return diff * diff;
+                    }
+                }).reduce(new Function2<Double, Double, Double>() {
+                    @Override
+                    public Double call(Double a, Double b) {
+                        return a + b;
+                    }
+                }) / data.count();
+        System.out.println("Test Mean Squared Error: " + testMSE);
+        System.out.println("Learned regression tree model:\n" + model.toDebugString());
+
+// Save and load model
+        model.save(jsc.sc(), "target/tmp/myDecisionTreeRegressionModel");
+        DecisionTreeModel sameModel = DecisionTreeModel
+                .load(jsc.sc(), "target/tmp/myDecisionTreeRegressionModel");
+    }
+}
